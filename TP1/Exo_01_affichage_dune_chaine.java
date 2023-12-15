0af0f2363c473d32503584802ce7a0666335d8ba
Ylorklan <yoanbor@icloud.com>
2023-12-15 16:26:40 +0100

    modification hello world

--- TP1/Exo_01_affichage_dune_chaine.java
+++ TP1/Exo_01_affichage_dune_chaine.java
@@ -3,7 +3,7 @@ package fr.yoanbor.TP1;
 public class Exo_01_affichage_dune_chaine {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World !");
 	}
 
 }
